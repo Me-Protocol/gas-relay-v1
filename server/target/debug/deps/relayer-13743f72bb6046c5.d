c1c65cd44136f2f2c6fce4ae89b65326aee08c8b
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-11-29 00:31:48 +0100

    mod: init server

---
+++ server/target/debug/deps/relayer-13743f72bb6046c5.d
@@ -0,0 +1,5 @@
+/Users/user/Documents/work/me-protocol/gasless-relayer/server/target/debug/deps/relayer-13743f72bb6046c5: bin/relayer/src/main.rs
+
+/Users/user/Documents/work/me-protocol/gasless-relayer/server/target/debug/deps/relayer-13743f72bb6046c5.d: bin/relayer/src/main.rs
+
+bin/relayer/src/main.rs:
---
+++ server/target/debug/relayer.d
@@ -0,0 +1,1 @@
+/Users/user/Documents/work/me-protocol/gasless-relayer/server/target/debug/relayer: /Users/user/Documents/work/me-protocol/gasless-relayer/server/bin/relayer/src/main.rs
