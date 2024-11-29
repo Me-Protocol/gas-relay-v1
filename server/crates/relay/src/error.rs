f89b6a00d3f7091fb96c24add27a6cdfc90410f5
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-11-29 10:08:40 +0100

    mod: introduced error for relay server

--- server/crates/relay/src/error.rs
+++ server/crates/relay/src/error.rs
@@ -1,1 +1,18 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 
+pub enum RelayServerError {
+    BadDataFormat,
+}
+
+impl IntoResponse for RelayServerError {
+    fn into_response(self) -> Response {
+        match self {
+            RelayServerError::BadDataFormat => {
+                (StatusCode::BAD_REQUEST, "Bad data format".to_string()).into_response()
+            }
+        }
+    }
+}
