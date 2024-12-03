02ff21c2ef32a0e1f6c0d8cff6d2afd4b7244755
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-12-03 18:17:18 +0100

    mod: binding task to cli module

--- server/bin/relayer/src/cli.rs
+++ server/bin/relayer/src/cli.rs
@@ -1,4 +1,18 @@
+use primitives::{configs::RelayerConfig, db::create_db_instance};
+use tasks::relay::ServerTask;
 use tracing_subscriber::{filter::LevelFilter, util::SubscriberInitExt};
+use clap::Parser;
+use toml::from_str;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct CliConfig {
+    #[arg(short, long)]
+    pub config_path: String,
+}
+
+
 
 /// Main entry point for the CLI
 ///
@@ -6,22 +20,24 @@ use tracing_subscriber::{filter::LevelFilter, util::SubscriberInitExt};
 /// Listens for a ctrl-c signal and shuts down all components when received.
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
     logger_setup()?;
-    // let config: Config = return_me_indexer_config();
+    let cli_config = CliConfig::parse();
+    let config_as_string = std::fs::read_to_string(cli_config.config_path)?;
+    let config: RelayerConfig = from_str(&config_as_string)?;
 
-    // // create system db is one has not been created
-    // let mut db_client = create_db_instance(&config.db_url)
-    //     .await
-    //     .expect("Could not create db instance");
-    // create_system_table(&mut db_client).await?;
+    // create system db is one has not been created
+    let mut db_client = create_db_instance(&config.server.db_url)
+        .await
+        .expect("Could not create db instance");
+    
 
-    // // server config
-    // let server_config = config.clone().server;
-    // //indexer config
+    // server config
+    let server_config = config.clone().server;
+    //indexer config
     // let indexer_configs = config.clone().indexer;
 
-    // tracing::info!("Starting Chronicle with config: {:?}", config.clone());
+    tracing::info!("Starting Relay with config: {:?}", config.clone());
 
-    // let mut tasks = vec![ServerTask::new(server_config, config.db_url.clone()).boxed()];
+    let mut tasks = vec![ServerTask::new(server_config).boxed()];
 
     // for indexer_config in indexer_configs {
     //     tasks.push(IndexerTask::new(indexer_config, config.db_url.clone()).boxed());
--- server/crates/primitives/src/configs.rs
+++ server/crates/primitives/src/configs.rs
@@ -9,6 +9,24 @@ pub struct ServerConfig {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RelayingAccount {
+    /// This is the URL of the server
+    pub name: String,
+    /// This is the URL of the database
+    pub private_key: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RelayerConfig {
+    /// This is the name of the chronicle server
+    pub name: Option<String>,
+    /// This is a list of all the indexer Config
+    pub relaying_accounts: Vec<RelayingAccount>,
+    /// Server config
+    pub server: ServerConfig,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Chain {
     pub chain_id: u64,
     pub chain_name: String,
