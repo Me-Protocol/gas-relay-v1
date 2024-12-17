02c937401ae81588d003fa25fde737723944208c
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-12-17 11:02:25 +0100

    mod: tx_receipt_result error mangagement

--- server/crates/monitor/src/lib.rs
+++ server/crates/monitor/src/lib.rs
@@ -19,16 +19,34 @@ pub async fn run_monitor_task(
 }
 
 pub async fn process_monitoring(ptx: PendingRequest, client: &tokio_postgres::Client) {
-    let tx_receipt = ptx.tx_pending.get_receipt().await.unwrap();
-    final_update_request_status(
-        client,
-        ptx.request_id,
-        RequestState::Success,
-        tx_receipt.block_number.unwrap_or_default(),
-        chrono::Utc::now().naive_utc(),
-        tx_receipt.gas_used as u64,
-        tx_receipt.transaction_hash.to_string(),
-    )
-    .await
-    .unwrap();
+    let tx_receipt_result = ptx.tx_pending.get_receipt().await;
+
+    match tx_receipt_result {
+        Ok(tx_receipt) => {
+            final_update_request_status(
+                client,
+                ptx.request_id,
+                RequestState::Success,
+                tx_receipt.block_number.unwrap_or_default(),
+                chrono::Utc::now().naive_utc(),
+                tx_receipt.gas_used as u64,
+                tx_receipt.transaction_hash.to_string(),
+            )
+            .await
+            .unwrap();
+        }
+        Err(e) => {
+            final_update_request_status(
+                client,
+                ptx.request_id,
+                RequestState::Failed,
+                0,
+                chrono::Utc::now().naive_utc(),
+                0 as u64,
+                e.to_string(),
+            )
+            .await
+            .unwrap();
+        }
+    }
 }
