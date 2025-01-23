2d6b6d99ab7f139acfe14a7329992c79863f0c69
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2025-01-23 18:09:08 +0100

    mod: added SDK intergration test

---
+++ sdk/ts/test/integration-test.ts
@@ -0,0 +1,51 @@
+import { batchedGaslessRelay, gaslessRelay, prepRequest } from '../src/relay'
+import { ethers } from 'ethers'
+
+const RELAYER_TEST_CONTRACT_ADDRESS = "0x5ff93753aaa767F6C65e2314d0Cd6A1a129eDCce";
+const DATA_FOR_TEST_CONTRACT = "0x82ab890a00000000000000000000000000000000000000000000000000000000000001f7";
+const DATA_FOR_TEST_CONTRACT_WITH_ERROR = "0xe736a0940000000000000000000000000000000000000000000000000000000000000017";
+const TRUSTED_FOWARDER = "0x2B3673aD2104b8E282F56a9f8a70cb45Ac11E97f";
+
+
+async function just_relayer() {
+    let provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545/');
+    let signer = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider); // this account those not have to gas payment token
+
+    let prepared_request = await prepRequest(
+        signer,
+        RELAYER_TEST_CONTRACT_ADDRESS,
+        BigInt(0),
+        DATA_FOR_TEST_CONTRACT,
+        TRUSTED_FOWARDER,
+        'ERC2771Forwarder',
+        BigInt(11155111),
+        '0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356'
+      );
+
+      let responds = await gaslessRelay(prepared_request, 'http://127.0.0.1:8010/relay');
+      console.log(responds);
+}
+
+async function relayer_with_error() {
+    let provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545/');
+    let signer = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider); // this account those not have to gas payment token
+
+    let prepared_request = await prepRequest(
+        signer,
+        RELAYER_TEST_CONTRACT_ADDRESS,
+        BigInt(0),
+        DATA_FOR_TEST_CONTRACT_WITH_ERROR,
+        TRUSTED_FOWARDER,
+        'ERC2771Forwarder',
+        BigInt(11155111),
+        '0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356'
+      );
+
+      let responds = await gaslessRelay(prepared_request, 'http://127.0.0.1:8010/relay');
+      console.log(responds);
+}
+
+async function main() {
+    await just_relayer();
+    // await relayer_with_error();
+}
